d189d69d55d27dec17ff8961d36527fa0e41bbec
GNU Support <github.com@gnu.support>
2025-03-08 17:24:52 +0300

    Update rcd-llm-speech-translate.sh

--- bin/rcd-llm-speech-translate.sh
+++ bin/rcd-llm-speech-translate.sh
@@ -5,39 +5,48 @@
 # initiates a process where the user can speak in English, and the
 # speech is then typed on the screen in the selected language.
 
-SCRIPT=$(yad --center \
-    --width=200 --height=400 \
-    --list \
-    --column="Languages to type for English speech" \
-    "Chinese" \
-    "Danish" \
-    "French" \
-    "German" \
-    "Italian" \
-    "Marathi" \
-    "Pidgin" \
-    "Portuguese" \
-    "Russian" \
-    "Spanish" \
-    "Swedish" \
-    --print-column=1 \
-    --separator="" \  # Set separator to empty string
-    --button="Run"
-    2>/dev/null)
+while true; do
+    SCRIPT=$(yad --center \
+        --title="Language Selection" \
+        --text="Select a language to type for English speech:" \
+        --width=300 --height=500 \
+        --list \
+        --column="Languages" \
+        "Chinese" \
+	"Croatian" \
+        "Danish" \
+        "French" \
+        "German" \
+        "Italian" \
+        "Marathi" \
+        "Pidgin" \
+        "Portuguese" \
+        "Russian" \
+        "Spanish" \
+        "Swedish" \
+        "Exit" \
+        --default-item="Italian" \
+        --print-column=1 \
+        --separator="" \
+        --button="Run" \
+        2>/dev/null)
 
-echo $SCRIPT
+    echo $SCRIPT >&2
 
-case $SCRIPT in
-    "Chinese")  bash rcd-llm-to-chinese.sh ;;
-    "Danish")  bash rcd-llm-to-danish.sh ;;
-    "French")  bash rcd-llm-to-french.sh ;;
-    "German")  bash rcd-llm-to-german.sh ;;
-    "Italian")  bash rcd-llm-to-italian.sh ;;
-    "Marathi")  bash rcd-llm-to-marathi.sh ;;
-    "Pidgin")  bash rcd-llm-to-pidgin.sh ;;
-    "Portuguese")  bash rcd-llm-to-portuguese.sh ;;
-    "Russian")  bash rcd-llm-to-russian.sh ;;
-    "Spanish") bash rcd-llm-to-spanish.sh ;;
-    "Swedish")  bash rcd-llm-to-swedish.sh ;;
-    *) echo "No script selected" ;;
-esac
+    case $SCRIPT in
+        "Chinese")  bash rcd-llm-to-chinese.sh ;;
+        "Danish")  bash rcd-llm-to-danish.sh ;;
+        "French")  bash rcd-llm-to-french.sh ;;
+        "German")  bash rcd-llm-to-german.sh ;;
+        "Italian")  bash rcd-llm-to-italian.sh ;;
+        "Marathi")  bash rcd-llm-to-marathi.sh ;;
+        "Pidgin")  bash rcd-llm-to-pidgin.sh ;;
+        "Portuguese")  bash rcd-llm-to-portuguese.sh ;;
+        "Croatian")  bash rcd-llm-to-croatian.sh ;;
+        "Russian")  bash rcd-llm-to-russian.sh ;;
+        "Spanish")  bash rcd-llm-to-spanish.sh ;;
+        "Swedish")  bash rcd-llm-to-swedish.sh ;;
+        "Exit")  exit 0 ;;
+        *) echo "No script selected" && exit 0 ;;
+    esac
+done
